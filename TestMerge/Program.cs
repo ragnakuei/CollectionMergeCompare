e9612ea0df47b0ec4360f0692bc48ddab86c8340
Kuei-PC\Kuei <ragnakuei@livemail.tw>
2017-08-10 09:10:28 +0800

    OK

---
+++ TestMerge/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace TestMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark>();
+            Console.ReadLine();
+        }
+    }
+
+    public class Benchmark
+    {
+        private readonly MyClass _benchClass = new MyClass();
+
+        [Benchmark]
+        public void 用Dictionary處理() => _benchClass.用Dictionary處理();
+
+        [Benchmark]
+        public void 用Zip處理() => _benchClass.用Zip處理();
+
+        [Benchmark]
+        public void 用ListWhere處理() => _benchClass.用ListWhere處理();
+    }
+
+    internal class MyClass
+    {
+        private readonly int _count = 100;
+
+        // 如果其中一個項目數量不同，最終結果就會不符合預期
+        public void 用Zip處理()
+        {
+            var names = Enumerable.Range(1, _count).Select(i => new NameClass { Id = i, Name = i.ToString() }).ToList();
+            var heights = Enumerable.Range(1, _count).Select(i => new HeightClass { Id = i, Height = i }).ToList();
+            var weights = Enumerable.Range(1, _count).Select(i => new WeightClass { Id = i, Wieght = i }).ToList();
+
+            var result1 = names.Zip(heights, (n, h) => new NameHeightWeightClass
+            {
+                Id = n.Id,
+                Name = n.Name,
+                Height = h.Height
+            });
+
+            var result2 = result1.Zip(weights, (n, w) => new NameHeightWeightClass
+            {
+                Id = n.Id,
+                Name = n.Name,
+                Height = n.Height,
+                Wieght = w.Wieght
+            });
+
+            var resultCount = result2.Count();
+        }
+
+        public void 用ListWhere處理()
+        {
+            var names = Enumerable.Range(1, _count).Select(i => new NameClass { Id = i, Name = i.ToString() }).ToList();
+            var heights = Enumerable.Range(1, _count).Select(i => new HeightClass { Id = i, Height = i }).ToList();
+            var weights = Enumerable.Range(1, _count).Select(i => new WeightClass { Id = i, Wieght = i }).ToList();
+
+            var result = names.Select(n =>
+            {
+                var height = heights.FirstOrDefault(h => h.Id == n.Id);
+                var weight = weights.FirstOrDefault(w => w.Id == n.Id);
+
+                return new NameHeightWeightClass
+                {
+                    Id = n.Id,
+                    Name = n.Name,
+                    Height = height.Height,
+                    Wieght = weight.Wieght
+                };
+            });
+
+            var resultCount = result.Count();
+        }
+
+        public void 用Dictionary處理()
+        {
+            var names = Enumerable.Range(1, _count).Select(i => new NameClass { Id = i, Name = i.ToString() });
+            var heights = Enumerable.Range(1, _count).Select(i => new HeightClass { Id = i, Height = i }).ToDictionary(k => k.Id, v => v);
+            var weights = Enumerable.Range(1, _count).Select(i => new WeightClass { Id = i, Wieght = i }).ToDictionary(k => k.Id, v => v);
+
+            var result = names.Select(n =>
+            {
+                var height = TryGetValue(heights, n.Id);
+                var weight = TryGetValue(weights, n.Id);
+
+                return new NameHeightWeightClass
+                {
+                    Id = n.Id,
+                    Name = n.Name,
+                    Height = height.Height,
+                    Wieght = weight.Wieght
+                };
+            });
+        }
+
+        private T2 TryGetValue<T1, T2>(Dictionary<T1, T2> dicts, T1 key)
+        {
+            T2 value;
+            dicts.TryGetValue(key, out value);
+            return value;
+        }
+    }
+
+    internal class NameClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    internal class HeightClass
+    {
+        public int Id { get; set; }
+        public int Height { get; set; }
+    }
+
+    internal class WeightClass
+    {
+        public int Id { get; set; }
+        public int Wieght { get; set; }
+    }
+
+    internal class NameHeightWeightClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public int Wieght { get; set; }
+    }
+}
